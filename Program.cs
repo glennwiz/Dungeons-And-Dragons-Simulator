80d383fcce73a68131dbe6b69b339532b001bbbc
glennwiz <glennwiz@gmail.com>
2012-04-14 14:41:02 +0200

    Added some Input logic at start

--- Program.cs
+++ Program.cs
@@ -9,23 +9,38 @@ namespace DND_Sim
 
         static void Main(string[] args)
         {
-            Logger.Verblog = false;
+            Console.Write("Number of Fights: > ");
+            num_figths = int.Parse(Console.ReadLine());
+
+            Console.Write("You want Verbose Output? > ");
+            string awnser = Console.ReadLine();
+
+            if (awnser[0] == 'y' || awnser[0] == 'Y')
+            {
+                Logger.Verblog = true;
+            }
+            else
+            {
+                Logger.Verblog = false;
+            }
 
             //creating a Archer
             var player1 = new Player {Name = "Archer", Ac = 7, Thac0 = 16, Hp = 12 };
-            Console.WriteLine("Player1 created: Name: {0} Armour: {1}, Health Points: {2}\n", player1.Name, player1.Ac,player1.Hp);
+            Logger.LogTrue(string.Format("Player1 created: Name: {0} Armour: {1}, Health Points: {2}", player1.Name, player1.Ac,player1.Hp), ConsoleColor.Green);
             
             //creating a 2d4 bow 
             var bow = new Bow {Name = "FireBow 2d4",DamageNum = 2, DamageSize = 4};
             player1.Wp = bow;
-            Console.WriteLine("{0} equipped\n", bow.Name);
+            Logger.LogTrue(string.Format("{0} equipped\n", bow.Name), ConsoleColor.Cyan);
 
             //creating an enemy 
             var enemy = new Enemy { Name = "Wolf", Ac = 6, Hp = 10, Thac0 = 10, Wp = null};
             var claw = new Weapon { Name = "Claw" , DamageNum = 1, DamageSize = 6};
             enemy.Wp = claw;
-            Console.WriteLine("{0} with damage {1}d{2} created\n",enemy.Name, claw.DamageNum,claw.DamageSize);
+            Logger.LogTrue(string.Format("{0} with damage {1}d{2} created",enemy.Name, claw.DamageNum,claw.DamageSize), ConsoleColor.Green);
 
+            Console.WriteLine("Press Any Key to start Simulation!!!");
+            Console.ReadKey();
             Attack(player1,enemy);
 
             Console.ReadLine();
